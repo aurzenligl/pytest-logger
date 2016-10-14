d4fd2717dc6c9a019980ba10948b9ca8b8e35e05
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2016-10-14 18:55:16 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='pytest-logger',
-    version='0.1',
+    version='0.1.1',
     author='Krzysztof Laskowski',
     author_email='krzysztof.laskowski@nokia.com',
     maintainer='Krzysztof Laskowski',
