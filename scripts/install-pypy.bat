d9f07d8f8fb08eee6d4304de41d5a04493f372fb
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2016-12-17 23:44:33 +0100

    appveyor config

---
+++ scripts/install-pypy.bat
@@ -0,0 +1,9 @@
+# script copied from pytest repository:
+# https://github.com/pytest-dev/pytest/blob/master/scripts/install-pypy.bat
+
+REM install pypy using choco
+REM redirect to a file because choco install python.pypy is too noisy. If the command fails, write output to console
+choco install python.pypy > pypy-inst.log 2>&1 || (type pypy-inst.log & exit /b 1)
+set PATH=C:\tools\pypy\pypy;%PATH%  # so tox can find pypy
+echo PyPy installed
+pypy --version
