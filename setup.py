322b8b99a1324cf1736853de0beea809d7758266
Krzysztof Laskowski <aurzenligl@gmail.com>
2024-02-21 01:10:37 +0100

    1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-logger',
-    version='1.0.0',
+    version='1.1.0',
     author='Krzysztof Laskowski',
     author_email='aurzenligl@gmail.com',
     maintainer='Krzysztof Laskowski',
